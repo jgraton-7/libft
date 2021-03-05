c99322f5c65796f5bf31c80ff6f355601c2ed0b9
user42 <jorge.graton8@gmail.com>
2021-03-05 21:33:31 +0100

    map-1

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstmap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jgraton- <jgraton-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/25 18:36:32 by jgraton-          #+#    #+#             */
-/*   Updated: 2021/02/25 18:37:03 by jgraton-         ###   ########.fr       */
+/*   Updated: 2021/03/05 21:32:47 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,14 @@
 
 t_list *ft_lstmap(t_list *lst, void *(*f)(void *),void (*del)(void *))
 {
-	
+	t_list *n_list;
+    t_list *end;
+    
+    end = NULL;
+    while(lst)
+    {
+        n_list = ft_lstnew((f)(lst->content));
+        ft_lstadd_back(&end, n_list);
+    }
+    return (end);
 }
