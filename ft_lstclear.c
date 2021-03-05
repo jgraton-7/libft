b690f725f0b2e5c025a1b1ddcc184cb099948824
user42 <jorge.graton8@gmail.com>
2021-03-05 20:03:56 +0100

    clear

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -3,19 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstclear.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jgraton- <jgraton-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/25 16:50:35 by jgraton-          #+#    #+#             */
-/*   Updated: 2021/02/25 18:28:02 by jgraton-         ###   ########.fr       */
+/*   Updated: 2021/03/05 19:35:17 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-// void ft_lstclear(t_list **lst, void (*del)(void*))
-// {
-// 	while()
-// 	{
-// 	}
-// }
+void ft_lstclear(t_list **lst, void (*del)(void*))
+{
+    t_list *tmp;
+
+    if(!lst || !*lst)
+	while(lst && *lst)
+	{
+        tmp = ((t_list*)*lst) -> next;
+        ft_lstdelone(lst, del);
+        lst = tmp;
+	}
+    free(tmp);
+}
